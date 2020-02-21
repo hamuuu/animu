08bc1f8d2e3e6850cf9281064c8521cc8301e43a
hamuuu <faldhisatria@gmail.com>
2020-02-21 18:51:56 +0700

    beta.1

--- src/Component/SideMenu/SocmedBar.js
+++ src/Component/SideMenu/SocmedBar.js
@@ -5,6 +5,19 @@ class SocmedBar extends React.Component {
     return(
       <div className="p-2">
         <div className="text-info border-bottom pb-2">Follow us!</div>
+        <div class="fb-page" data-href="https://web.facebook.com/Indonesia.AO/"
+              data-tabs=""
+              data-width=""
+              data-height=""
+              data-small-header="false"
+              data-adapt-container-width="true"
+              data-hide-cover="false"
+              data-show-facepile="true"
+          >
+            <blockquote cite="https://web.facebook.com/Indonesia.AO/" class="fb-xfbml-parse-ignore">
+              <a href="https://web.facebook.com/Indonesia.AO/">Atlantica Online Indonesia ( Official )</a>
+            </blockquote>
+        </div>
       </div>
     )
   }
